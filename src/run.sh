098971247fe0dc58b1c80360d47ff69952d21d7c
Hoda Kermani <kermanihoda@gmail.com>
2018-12-01 19:51:19 +0330

    input file

--- src/run.sh
+++ src/run.sh
@@ -13,4 +13,4 @@ find . -type f -name "*.class" -delete
 java -jar /usr/local/lib/antlr-4.7.1-complete.jar Smoola.g4
 find . -name "*.java" > sources.txt
 javac @sources.txt
-java org.antlr.v4.gui.TestRig Smoola program < ../../smoolaPhase3.iml
+java org.antlr.v4.gui.TestRig Smoola program < ../in.sml
